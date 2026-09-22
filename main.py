d884a0159541fbccb62be39b24459a8e1a4ba853
Elliahna Pham <epham29@setoncatholic.org>
2026-09-22 18:08:01 +0000

    number game

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+game_number = random.randint(1,10)
+print(game_number)
+while(True):
+    guess = int(input("Enter a number between 1 and 10: "))
+
+
+    if guess > game_number:
+        print("Too High")
+    elif guess < game_number:
+        print("Too Low")
+    else:
+        print("You Win!")
+        break
