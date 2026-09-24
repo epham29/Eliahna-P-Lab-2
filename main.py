4fe786d5ec63548a4d9e79ad64f865dc984052ea
Elliahna Pham <epham29@setoncatholic.org>
2026-09-24 17:33:02 +0000

    commit 2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
 
 game_number = random.randint(1,10)
-print(game_number)
+#print(game_number)
+
+attempts = 0
+
 while(True):
     guess = int(input("Enter a number between 1 and 10: "))
-
+    attempts += 1
 
     if guess > game_number:
         print("Too High")
@@ -12,4 +15,8 @@ while(True):
         print("Too Low")
     else:
         print("You Win!")
+        if attempts >= 5:
+            print("Do better next time!")
+        else:
+            print("Great job!")
         break
